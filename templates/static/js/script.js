ccf0175638cd73437c5fafaea28b1d809916ebc5
clanofnoobs <samiulg3@gmail.com>
2014-03-12 13:24:31 -0400

    adjusting album

--- templates/static/js/script.js
+++ templates/static/js/script.js
@@ -16,6 +16,19 @@ $(function myFunction (){
 
 
 });
+$(function (){
+$("#portfolio img").click(function(){
+        var src = $(this).attr("src");
+        $("#frame img").attr("src", src); 
+        $("#frame").fadeIn();
+        $("#overlay").fadeIn();
+});
+$("#overlay").click(function(){
+        $("#frame").fadeOut();
+        $("#overplay").fadeOut();
+});
+        
+});
 
 $(function (){
  $("#menub").hover(function (){
