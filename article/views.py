439b61aa628f0ba12f829695ac82b172df17bda9
clanofnoobs <samiulg3@gmail.com>
2014-05-31 12:54:07 -0400

    REST, status codes

--- article/views.py
+++ article/views.py
@@ -14,6 +14,7 @@ from rest_framework.renderers import JSONRenderer
 from rest_framework.parsers import JSONParser
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework import status
 
 
 @api_view(['GET','POST'])
